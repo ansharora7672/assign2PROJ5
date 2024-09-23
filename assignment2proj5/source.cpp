686a0da3e082350f364ddb3ea40638f9470ced34
arora <arora@ANSH>
2024-09-23 09:24:53 -0400

    Add project files.

---
+++ assignment2proj5/source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+struct STUDENT_DATA {
+	string firstname;
+	string lastname;
+};
+
+int main() {
+
+	ifstream file("StudentData.txt");
+
+	vector<STUDENT_DATA> student_data;
+	string line;
+
+	while (getline(file, line))
+	{
+		istringstream iss(line);
+		STUDENT_DATA student;
+		getline(iss, student.firstname, ',');
+		getline(iss, student.lastname);
+		student_data.push_back(student);
+	}
+
+	file.close();
+
+	return 1;
+
+}
